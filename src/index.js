59d99d2d7cf11891067426c16a15d3303ade27d7
MyMcgit <109052841+MyMcgit@users.noreply.github.com>
2023-04-13 22:02:34 +0800

    暂时提交

--- src/index.js
+++ src/index.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter}from 'react-router-dom'
-import {ConfigProvider} from 'antd'
+import { BrowserRouter } from 'react-router-dom'
+import { ConfigProvider } from 'antd'
 import App from './App';
+
+import { Provider } from 'react-redux';
 // 自定义主体：采用ConfigProvider组件和theme
 const defaultData = {
-  borderRadius: 6,
-  colorPrimary: 'rgb(0, 154, 126 )',
+    borderRadius: 6,
+    colorPrimary: 'rgb(0, 154, 126 )',
 };
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-  <React.StrictMode>
-    <BrowserRouter>
-      <ConfigProvider  theme={{
-        token: {
-          colorPrimary: defaultData.colorPrimary,
-          borderRadius: defaultData.borderRadius,
-        },
-      }}>
-        <App />
-      </ConfigProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+
+    <React.StrictMode >
+    <Provider > </Provider>
+    </React.StrictMode>
 );
